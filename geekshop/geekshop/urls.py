8e6ff72df97bd789327d8517db01ba3b379c5574
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-01-06 01:04:22 +0300

    Add base template

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main),
-    path('products/', mainapp.products),
-    path('contacts/', mainapp.contacts),
+    path('', mainapp.main, name='main'),
+    path('products/', mainapp.products, name='products'),
+    path('contacts/', mainapp.contacts, name='contacts'),
     path('admin/', admin.site.urls),
 ]
