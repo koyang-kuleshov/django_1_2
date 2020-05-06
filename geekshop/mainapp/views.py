ed641c067e87cf1fb71a0bd8a7288410e8f4ff4c
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-05-06 23:22:05 +0300

    Сделал вывод всех продуков из БД

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,9 +1,9 @@
 import random
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ProductCategory, Product
 
 
-def main(requests):
+def main(request):
     spam = list(Product.objects.all())
     random.shuffle(spam)
     product_list = spam[:4]
@@ -16,19 +16,37 @@ def main(requests):
         'trending_products': trending_products,
         'small_products': small_products,
     }
-    return render(requests, 'mainapp/index.html', context=context)
+    return render(request, 'mainapp/index.html', context=context)
 
 
-def products(requests, pk=None):
+def products(request, pk=None):
     title = 'Продукты интернет-магазина'
     links_menu = ProductCategory.objects.all()
     same_products = Product.objects.all()[:3]
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all()
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk)
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'same_products': same_products,
+            'category': category,
+            'products': products,
+        }
+        return render(request, 'mainapp/products_list.html', context)
+
     context = {
         'title': title,
         'links_menu': links_menu,
         'same_products': same_products,
     }
-    return render(requests, 'mainapp/products.html', context=context)
+
+    return render(request, 'mainapp/products.html', context=context)
 
 
 def contacts(request):
