64a52aa1b853dcb7376df4098d092ff68134c204
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-05-19 00:04:21 +0300

    Настроил инстурменты для отладки

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+# from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
@@ -13,9 +14,14 @@ urlpatterns = [
     path('contacts/', mainapp.contacts, name='contacts'),
     path('admin/', include('adminapp.urls', namespace='admin')),
     path('order/', include('ordersapp.urls', namespace='order')),
-    path('', include('social_django.urls', namespace='social'))
+    path('', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    # urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
